Fix yaw rotation normalisation range to -1 to 1 and fix end_rmse

source/extensions/omni.isaac.lab/omni/isaac/lab/envs/direct_rl_env_feedback.py
@@ -315,6 +315,33 @@ class DirectRLEnvFeedback(DirectRLEnv):
         self.reset_buf = self.reset_terminated | self.reset_time_outs
         self.reward_buf = self._get_rewards()
 
+        success_tensor = self.goal_reached
+        failed_tensor = self.episode_failed
+
+        # Update record_tensor based on success_tensor and failed_tensor
+        self.success_record = torch.where(success_tensor, torch.tensor(True, dtype=torch.bool), self.success_record)
+        self.success_record = torch.where(failed_tensor, torch.tensor(False, dtype=torch.bool), self.success_record)
+
+        end_rmse_record_mean = 0.0
+        if "curr_rmse" in done_info: 
+            curr_rmse = done_info["curr_rmse"]
+            # print(curr_rmse.shape)
+            end_tensor = success_tensor | failed_tensor
+            # self.end_rmse_record = torch.where(end_tensor, torch.tensor(True, dtype=torch.bool), self.success_record)
+            self.end_rmse_record = torch.where(end_tensor, curr_rmse, self.end_rmse_record)
+            # print("End")
+            # print(end_tensor)
+            # print(curr_rmse)
+            # print(self.end_rmse_record)
+            # print(self.end_rmse_record)
+            # print(self.end_rmse_record.mean())
+            end_rmse_record_mean = self.end_rmse_record.mean()
+            # print(end_rmse_record_mean)
+
+            self.extras["prop_estimation"] = {
+                "curr_rmse": done_info["curr_rmse"], 
+            } 
+
         # print("transition_to_task_idx")
         # print(self.transition_to_task_idx)
         if self.transition_to_task_idx is not None: 
@@ -345,28 +372,28 @@ class DirectRLEnvFeedback(DirectRLEnv):
         # self.goal_reached
         # print(self.episode_failed)      
 
-        success_tensor = self.goal_reached
-        failed_tensor = self.episode_failed
+        # success_tensor = self.goal_reached
+        # failed_tensor = self.episode_failed
 
-        # Update record_tensor based on success_tensor and failed_tensor
-        self.success_record = torch.where(success_tensor, torch.tensor(True, dtype=torch.bool), self.success_record)
-        self.success_record = torch.where(failed_tensor, torch.tensor(False, dtype=torch.bool), self.success_record)
+        # # Update record_tensor based on success_tensor and failed_tensor
+        # self.success_record = torch.where(success_tensor, torch.tensor(True, dtype=torch.bool), self.success_record)
+        # self.success_record = torch.where(failed_tensor, torch.tensor(False, dtype=torch.bool), self.success_record)
 
-        end_rmse_record_mean = 0.0
-        if "curr_rmse" in done_info: 
-            curr_rmse = done_info["curr_rmse"]
-            # print(curr_rmse.shape)
-            end_tensor = success_tensor | failed_tensor
-            # print(end_tensor.shape)
-            # self.end_rmse_record = torch.where(end_tensor, torch.tensor(True, dtype=torch.bool), self.success_record)
-            self.end_rmse_record = torch.where(end_tensor, curr_rmse, self.end_rmse_record)
-            # print(self.end_rmse_record)
-            # print(self.end_rmse_record.mean())
-            end_rmse_record_mean = self.end_rmse_record.mean()
+        # end_rmse_record_mean = 0.0
+        # if "curr_rmse" in done_info: 
+        #     curr_rmse = done_info["curr_rmse"]
+        #     # print(curr_rmse.shape)
+        #     end_tensor = success_tensor | failed_tensor
+        #     # print(end_tensor.shape)
+        #     # self.end_rmse_record = torch.where(end_tensor, torch.tensor(True, dtype=torch.bool), self.success_record)
+        #     self.end_rmse_record = torch.where(end_tensor, curr_rmse, self.end_rmse_record)
+        #     # print(self.end_rmse_record)
+        #     # print(self.end_rmse_record.mean())
+        #     end_rmse_record_mean = self.end_rmse_record.mean()
 
-            self.extras["prop_estimation"] = {
-                "curr_rmse": done_info["curr_rmse"], 
-            } 
+        #     self.extras["prop_estimation"] = {
+        #         "curr_rmse": done_info["curr_rmse"], 
+        #     } 
 
         if "goal_bounds_exp" in done_info: 
             self.extras["prop_estimation"]["goal_bounds_exp"] = done_info["goal_bounds_exp"]

source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/exploration/exploration_combined_env.py
@@ -291,8 +291,11 @@ class ExplorationCombinedEnv(DirectRLEnvFeedback):
         self.object_location_normmin = -2.0
         self.object_vel_normmax = 3.0
         self.object_vel_normmin = -3.0
-        self.object_rot_normmax = 2.0
-        self.object_rot_normmin = -2.0
+        self.object_rot_normmax = 1.0
+        self.object_rot_normmin = -1.0
+
+        self.yaw_min_check = 1000000
+        self.yaw_max_check = -1000000
 
         # Success rate tracking
         self.success_rates = []
@@ -618,7 +621,28 @@ class ExplorationCombinedEnv(DirectRLEnvFeedback):
 
         # Offline prop estimation
 
-        curr_obs_prop = torch.cat((normalized_past_puck_pos_obs_x, normalized_past_puck_pos_obs_y, curr_cylinderpuck2_state[:, 6].view(-1,1), normalized_past_pusher_pos_obs_x, normalized_past_pusher_pos_obs_y), dim=1)
+        # print("Rot")
+        # print(curr_cylinderpuck2_state[:, 6].view(-1,1))
+        # print("Normalised yaw")
+        # print(normalized_past_puck_rot_obs_yaw)
+        # if torch.min(curr_cylinderpuck2_state[:, 6]) < self.yaw_min_check: 
+        #     self.yaw_min_check = torch.min(curr_cylinderpuck2_state[:, 6]) 
+        # if torch.max(curr_cylinderpuck2_state[:, 6]) > self.yaw_min_check: 
+        #     self.yaw_max_check = torch.max(curr_cylinderpuck2_state[:, 6]) 
+        # Get min and max of the specific column once
+        # min_yaw = torch.min(curr_cylinderpuck2_state[:, 6])
+        # max_yaw = torch.max(curr_cylinderpuck2_state[:, 6])
+        # if min_yaw < self.yaw_min_check:
+        #     self.yaw_min_check = min_yaw.item()  # Use .item() to convert tensor to Python number
+
+        # if max_yaw > self.yaw_max_check:
+        #     self.yaw_max_check = max_yaw.item()  # Use .item() to convert tensor to Python number
+        # print("Yaw min max check")
+        # print(self.yaw_min_check)
+        # print(self.yaw_max_check)
+        
+        # curr_obs_prop = torch.cat((normalized_past_puck_pos_obs_x, normalized_past_puck_pos_obs_y, curr_cylinderpuck2_state[:, 6].view(-1,1), normalized_past_pusher_pos_obs_x, normalized_past_pusher_pos_obs_y), dim=1)
+        curr_obs_prop = torch.cat((normalized_past_puck_pos_obs_x, normalized_past_puck_pos_obs_y, normalized_past_puck_rot_obs_yaw, normalized_past_pusher_pos_obs_x, normalized_past_pusher_pos_obs_y), dim=1)
         self.past_obs_prop.append(curr_obs_prop.unsqueeze(0))
         self.past_obs_prop = self.past_obs_prop[-self.past_timestep_prop:]
         past_obs_prop_tensor = torch.cat(self.past_obs_prop, dim=0)

source/standalone/workflows/skrl/play2_feedback_propexp.py
@@ -260,6 +260,11 @@ def main():
         config={},
     )
 
+    # Test mode
+    # test_mode = "taskonly"
+    # # test_mode = "exptask"
+    test_mode = env.test_mode
+
     # reset environment
     obs, infos = env.reset()
     prev_total_episode_num = 0
@@ -309,8 +314,9 @@ def main():
                 # print(task_phase)
                 # print("Current timestep")
                 # print(env.episode_length_buf.shape)
-                # curr_task_phase = torch.ones(env.num_envs, dtype=torch.bool).to(env.device)
-                # curr_task_phase = env.episode_length_buf > 5
+                if test_mode == "taskonly": 
+                    # curr_task_phase = torch.ones(env.num_envs, dtype=torch.bool).to(env.device)
+                    curr_task_phase = env.episode_length_buf > 5
                 # print(infos["prop_estimation"]["curr_rmse"])
                 # print(obs)
                 # print(curr_task_phase)
@@ -320,7 +326,7 @@ def main():
             transition_to_task_idx = torch.nonzero(transition_to_task).squeeze().view(-1)
             env._get_transition_to_task_idx(transition_to_task_idx)
 
-            curr_prop_info = env._get_estimation()["denormalsied_output"]
+            curr_prop_info = env._get_estimation()["denormalsied_target"]
             # print("prop")
             # print(curr_task_phase)
             if curr_prop_info is not None: 
@@ -338,14 +344,15 @@ def main():
                 # Normalize the entire tensor (broadcasting works here)
                 normalised_expend_prop_info = normalize(expend_prop_info, dynamic_frictions_min, dynamic_frictions_max, state_norm_min, state_norm_max)
                 
-            # print("Check")
-            # print(curr_task_phase)
-            # print(obs[:,11])
+            # # print("Check")
+            # # print(curr_task_phase)
+            # # print(obs[:,11])
             obs_task = obs
-            if normalised_expend_prop_info is not None: 
-                obs_task[curr_task_phase, 11] = normalised_expend_prop_info[curr_task_phase, 0]
+            if test_mode != "taskonly": 
+                if normalised_expend_prop_info is not None: 
+                    obs_task[curr_task_phase, 11] = normalised_expend_prop_info[curr_task_phase, 0]
 
-            # print(obs_task[:,11])
+            # # print(obs_task[:,11])
 
             # Compute action
             # actions = agent.act(obs, timestep=0, timesteps=0)[0]
@@ -356,6 +363,8 @@ def main():
             obs_exp = obs[:, :exp_observation_space]
             actions_exp, log_prob_exp, outputs_exp, prop_estimator_output_exp = exp_agent.act(obs_exp, infos, timestep=0, timesteps=0)
             actions_exp = outputs_exp["mean_actions"]
+            if test_mode == "taskonly": 
+                actions_exp = torch.zeros_like(actions_exp)
 
             final_actions = torch.where(curr_task_phase.unsqueeze(1), actions_task, actions_exp)
             # final_actions = torch.where(task_phase.unsqueeze(1), actions_task, actions_rest)
@@ -457,7 +466,7 @@ def main():
                 # wandb.log({"Episode_num": total_episode_num})  
                 # wandb.log({"success_rate": success_rate_1env}) 
 
-                print(curr_task_change)
+                # print(curr_task_change)
                 if curr_task_change: 
                     task_change_num+=1
                 else: 
@@ -467,8 +476,8 @@ def main():
                 # print(task_change_num)
                 # print(task_change_rate)
                 curr_task_change = False
-                print("Exp failed num")
-                print(exp_failed_num)
+                # print("Exp failed num")
+                # print(exp_failed_num)
 
                 if "log" in infos and "end_rmse" in infos["log"]:
                     end_rmse = infos["log"]["end_rmse"]